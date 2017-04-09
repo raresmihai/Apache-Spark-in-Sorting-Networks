98e34b2e4f1de0c0e160afe04ad2f3deb00be967
raresmihai <rares.babuta@info.uaic.ro>
2017-04-09 13:33:25 +0300

    Modified properties

--- src/main/java/network/NetworkProperties.java
+++ src/main/java/network/NetworkProperties.java
@@ -9,5 +9,5 @@ public class NetworkProperties implements Serializable{
     private static final int comparatorsOptimalSize[] = {0,0,1,3,5,9,12,16,19,25,29,33,37,41,45,49,53};
     public static final int NUMBER_OF_WIRES = 9;
     public static final int NUMBER_OF_COMPARATORS = comparatorsOptimalSize[NUMBER_OF_WIRES];
-    public static final int NUMBER_OF_SEQ_GENERATE_STEPS = 0;
+    public static final int NUMBER_OF_SEQ_GENERATE_STEPS = 1;
 }
